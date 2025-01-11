143a0b621a50453dab457923d32fa1f934d1fb5c
Hajar <hajar.houari@edu.devinci.fr>
2025-01-12 00:04:34 +0100

    play ajout scores, coups, etc

--- front-end-chifoumi/src/pages/Play.jsx
+++ front-end-chifoumi/src/pages/Play.jsx
@@ -24,19 +24,6 @@ function Play() {
     };
 
     fetchGameState();
-
-    const eventSource = new EventSource(
-      `http://fauques.freeboxos.fr:3000/matches/${matchId}/subscribe?token=${token}`
-    );
-
-    eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      if (data.type === "NEW_TURN" || data.type === "TURN_ENDED") {
-        fetchGameState();
-      }
-    };
-
-    return () => eventSource.close();
   }, [matchId]);
 
   const handlePlayMove = async (move) => {
@@ -67,7 +54,13 @@ function Play() {
         {gameState ? (
           <Box>
             <Typography variant="body1">
-              Current State: {gameState.state}
+              Player 1: {gameState.user1.username} | Score: {gameState.user1.score}
+            </Typography>
+            <Typography variant="body1">
+              Player 2: {gameState.user2?.username || "Waiting"} | Score: {gameState.user2?.score || 0}
+            </Typography>
+            <Typography variant="body1">
+              Current Turn: {gameState.turns.length + 1}
             </Typography>
             <Button
               fullWidth
