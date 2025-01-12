b74272465f439eb0368abb0b8539f3b15a311af8
Ayoubion <ayoub924000@gmail.com>
2025-01-12 11:16:36 +0100

    Correction watch Dans route

--- front-end-chifoumi/src/App.jsx
+++ front-end-chifoumi/src/App.jsx
@@ -1,18 +1,18 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Lobby from "./pages/Lobby";
 import Play from "./pages/Play";
+import Watch from "./pages/Watch";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route pour la page de connexion */}
-        <Route path="/" element={<Login />} />
-        {/* Route pour la page du lobby */}
+        <Route path="/login" element={<Login />} />
         <Route path="/lobby" element={<Lobby />} />
-        {/* Route pour la page de jeu */}
         <Route path="/play/:matchId" element={<Play />} />
+        <Route path="/watch/:matchId" element={<Watch />} />
       </Routes>
     </Router>
   );
